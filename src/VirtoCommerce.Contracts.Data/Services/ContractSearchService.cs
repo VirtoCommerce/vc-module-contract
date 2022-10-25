d84ea5b15bc1a80500f97ade6d81cf71342095be
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2022-10-25 10:26:50 +0200

    PT-8737, PT-9306: add contract search feature (#7)
    * feat: add contract search feature
    
    * fix: fix empty product search result errors

--- src/VirtoCommerce.Contracts.Data/Services/ContractSearchService.cs
+++ src/VirtoCommerce.Contracts.Data/Services/ContractSearchService.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using VirtoCommerce.Contracts.Core.Models;
 using VirtoCommerce.Contracts.Core.Models.Search;
 using VirtoCommerce.Contracts.Data.Models;
@@ -9,17 +10,24 @@ using VirtoCommerce.Platform.Core.Caching;
 using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Platform.Core.GenericCrud;
 using VirtoCommerce.Platform.Data.GenericCrud;
+using VirtoCommerce.StoreModule.Core.Model;
+using VirtoCommerce.StoreModule.Core.Model.Search;
+using PredicateBuilder = VirtoCommerce.Platform.Core.Common.PredicateBuilder;
 
 namespace VirtoCommerce.Contracts.Data.Services
 {
     public class ContractSearchService : SearchService<ContractSearchCriteria, ContractSearchResult, Contract, ContractEntity>
     {
+        readonly ISearchService<StoreSearchCriteria, StoreSearchResult, Store> _storeSearchService;
+
         public ContractSearchService(
             Func<IContractRepository> repositoryFactory,
             IPlatformMemoryCache platformMemoryCache,
-            ICrudService<Contract> service)
+            ICrudService<Contract> service,
+            ISearchService<StoreSearchCriteria, StoreSearchResult, Store> storeSearchService)
             : base(repositoryFactory, platformMemoryCache, service)
         {
+            _storeSearchService = storeSearchService;
         }
 
         protected override IQueryable<ContractEntity> BuildQuery(IRepository repository, ContractSearchCriteria criteria)
@@ -31,9 +39,11 @@ namespace VirtoCommerce.Contracts.Data.Services
                 query = query.Where(x => criteria.ObjectIds.Contains(x.Id));
             }
 
+            // search contract name OR contract code OR store name by an expression
             if (!string.IsNullOrEmpty(criteria.Keyword))
             {
-                query = query.Where(x => x.Name.Contains(criteria.Keyword) || x.Description.Contains(criteria.Keyword));
+                var predicate = GetKeywordPredicate(criteria.Keyword);
+                query = query.Where(predicate);
             }
 
             if (!string.IsNullOrEmpty(criteria.StoreId))
@@ -53,6 +63,36 @@ namespace VirtoCommerce.Contracts.Data.Services
             }
 
             return query;
+        }
+
+        /// <summary>
+        /// Find store by name and create predicate with Name and Code conditions
+        /// </summary>
+        private Expression<Func<ContractEntity, bool>> GetKeywordPredicate(string keyword)
+        {
+            var predicate = PredicateBuilder.False<ContractEntity>();
+
+            predicate = PredicateBuilder.Or(predicate, x => x.Name.Contains(keyword));
+            predicate = PredicateBuilder.Or(predicate, x => x.Code.Contains(keyword));
+
+            // find storeIds by keyword and add them to the predicate
+            var storeSearchCriteria = AbstractTypeFactory<StoreSearchCriteria>.TryCreateInstance();
+            storeSearchCriteria.Keyword = keyword;
+            storeSearchCriteria.ResponseGroup = StoreResponseGroup.StoreInfo.ToString();
+            storeSearchCriteria.Take = 0;
+
+            var storeCountResult = _storeSearchService.SearchAsync(storeSearchCriteria).GetAwaiter().GetResult();
+
+            if (storeCountResult.TotalCount > 0)
+            {
+                storeSearchCriteria.Take = storeCountResult.TotalCount;
+                var storeSearchResult = _storeSearchService.SearchAsync(storeSearchCriteria).GetAwaiter().GetResult();
+
+                var storeIds = storeSearchResult.Results.Select(x => x.Id).ToList();
+                predicate = PredicateBuilder.Or(predicate, x => storeIds.Contains(x.StoreId));
+            }
+
+            return predicate;
         }
 
         protected override IList<SortInfo> BuildSortExpression(ContractSearchCriteria criteria)
--- src/VirtoCommerce.Contracts.Web/Scripts/blades/contracts-list.js
+++ src/VirtoCommerce.Contracts.Web/Scripts/blades/contracts-list.js
@@ -55,6 +55,7 @@ angular.module('Contracts')
 
                 function getSearchCriteria() {
                     return {
+                        keyword: filter.keyword ? filter.keyword : undefined,
                         sort: uiGridHelper.getSortExpression($scope),
                         skip: ($scope.pageSettings.currentPage - 1) * $scope.pageSettings.itemsPerPageCount,
                         take: $scope.pageSettings.itemsPerPageCount
